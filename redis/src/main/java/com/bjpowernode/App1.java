4bdcef8797db0650a512501e2fd1cc6f28e8442f
SYJoaa <wfsgsyj@gmail.com>
2021-08-10 19:11:19 +0800

    77

--- redis/src/main/java/com/bjpowernode/App1.java
+++ redis/src/main/java/com/bjpowernode/App1.java
@@ -21,30 +21,3 @@ public class App1 {
         }
     }
 }
-
-class MyDemo {
-    private static ThreadLocal<String> tl = new ThreadLocal<>();
-    private String content;
-
-    private String getContent() {
-        return tl.get();
-    }
-    private void setContent(String content) {
-        tl.set(content);
-    }
-    public static void main(String[] args) {
-        MyDemo demo = new MyDemo();
-        for (int i = 0; i < 5; i++) {
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    demo.setContent(Thread.currentThread().getName() + "的数据");
-                    System.out.println("-----------------------");
-                    System.out.println(Thread.currentThread().getName() + "--->" + demo.getContent());
-                }
-            });
-            thread.setName("线程" + i);
-            thread.start();
-        }
-    }
-}
